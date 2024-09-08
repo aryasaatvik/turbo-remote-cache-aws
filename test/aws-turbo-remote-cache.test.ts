695ab529665b484d9fce1942b90c01e7917565e3
Saatvik Arya <aryasaatvik@gmail.com>
2024-09-07 17:24:39 -0700

    Initial commit

---
+++ test/aws-turbo-remote-cache.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsTurboRemoteCache from '../lib/aws-turbo-remote-cache-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-turbo-remote-cache-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsTurboRemoteCache.AwsTurboRemoteCacheStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
