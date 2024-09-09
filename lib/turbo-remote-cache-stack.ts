11cb9a459278d2ff105081ac4ea983a537e20024
Saatvik Arya <aryasaatvik@gmail.com>
2024-09-08 23:26:28 -0700

    s3 iam role, add s3 expiration lifecycle rule, and add logs for api gateway

--- lib/turbo-remote-cache-stack.ts
+++ lib/turbo-remote-cache-stack.ts
@@ -2,28 +2,39 @@ import * as cdk from 'aws-cdk-lib';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as logs from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
 import * as path from 'path';
+import * as dotenv from 'dotenv';
 
 export class TurboRemoteCacheStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
     const artifactsBucket = new s3.Bucket(this, 'ArtifactsBucket', {
-      versioned: true,
+      versioned: false,
       encryption: s3.BucketEncryption.S3_MANAGED,
-      cors: [
+      lifecycleRules: [
         {
-          allowedMethods: [
-            s3.HttpMethods.GET,
-            s3.HttpMethods.PUT,
-            s3.HttpMethods.HEAD,
-          ],
-          allowedOrigins: ['*'],
-          allowedHeaders: ['*'],
+          expiration: cdk.Duration.days(30),
         },
       ],
     });
+
+    const s3Credentials = new iam.Role(this, 'S3CredentialsRole', {
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal('apigateway.amazonaws.com'),
+        new iam.AccountPrincipal(this.account)
+      ),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'),
+      ],
+    });
+
+    artifactsBucket.grantReadWrite(s3Credentials);
 
     const recordEventsFunction = new lambda.Function(this, 'RecordEventsFunction', {
       runtime: lambda.Runtime.NODEJS_20_X,
@@ -87,10 +98,26 @@ export class TurboRemoteCacheStack extends cdk.Stack {
     artifactsBucket.grantReadWrite(recordEventsFunction);
     artifactsBucket.grantRead(statusFunction);
 
+    const logGroup = logs.LogGroup.fromLogGroupName(this, 'LogGroup', '/aws/apigateway/turbo-remote-cache-api');
+
     // Create API Gateway
     const api = new apigateway.RestApi(this, 'TurboRemoteCacheApi', {
       restApiName: 'Turbo Remote Cache API',
       description: 'API for Turborepo Remote Cache',
+      cloudWatchRole: true,
+      deployOptions: {
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields(),
+        dataTraceEnabled: true,
+        tracingEnabled: true,
+      },
+      binaryMediaTypes: ['application/octet-stream'],
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: apigateway.Cors.DEFAULT_HEADERS,
+      },
     });
 
     // Create resources and methods
