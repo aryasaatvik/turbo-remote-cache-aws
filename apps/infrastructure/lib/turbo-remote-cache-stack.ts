07ae85bb166d2bc1ed729aedf26085f0b4840b87
Saatvik Arya <aryasaatvik@gmail.com>
2024-09-18 12:39:35 -0700

    rename construct package

--- apps/infrastructure/lib/turbo-remote-cache-stack.ts
+++ apps/infrastructure/lib/turbo-remote-cache-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { TurboRemoteCache } from '../../../packages/construct/src';
+import { TurboRemoteCache } from 'turbo-remote-cache-construct';
 
 export class TurboRemoteCacheStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
