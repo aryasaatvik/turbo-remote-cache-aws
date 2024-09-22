21911a1af769ffecd679c9c2a90a013801025dbb
Saatvik Arya <aryasaatvik@gmail.com>
2024-09-22 15:45:21 -0700

    exclude template

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   projects: [
-    '<rootDir>/apps/*/jest.config.js',
+    '<rootDir>/apps/infrastructure/jest.config.js',
     '<rootDir>/packages/*/jest.config.js',
   ],
   coverageDirectory: '<rootDir>/coverage/',
