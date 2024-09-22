e7c2b342db6417aafbf881dc2220347e5c8ea087
Saatvik Arya <aryasaatvik@gmail.com>
2024-09-22 00:50:17 -0700

    disable login related endpoints

--- packages/construct/src/lambda.ts
+++ packages/construct/src/lambda.ts
@@ -51,32 +51,33 @@ export class LambdaFunctions extends Construct {
     });
 
     // turbo login
-    this.initiateLoginFunction = new lambda.Function(this, 'InitiateLoginFunction', {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      functionName: 'turbo-remote-cache-initiate-login',
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/dist/initiate-login')),
-      environment: {
-        TURBO_TOKEN: process.env.TURBO_TOKEN!,
-      },
-    });
+    // TODO: implement lambda authorizer to validate third party JWT
+    // this.initiateLoginFunction = new lambda.Function(this, 'InitiateLoginFunction', {
+    //   runtime: lambda.Runtime.NODEJS_20_X,
+    //   functionName: 'turbo-remote-cache-initiate-login',
+    //   handler: 'index.handler',
+    //   code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/dist/initiate-login')),
+    //   environment: {
+    //     TURBO_TOKEN: process.env.TURBO_TOKEN!,
+    //   },
+    // });
 
-    this.loginSuccessFunction = new lambda.Function(this, 'LoginSuccessFunction', {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      functionName: 'turbo-remote-cache-login-success',
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/dist/login-success')),
-      environment: {
-        TURBO_TOKEN: process.env.TURBO_TOKEN!,
-      },
-    });
+    // this.loginSuccessFunction = new lambda.Function(this, 'LoginSuccessFunction', {
+    //   runtime: lambda.Runtime.NODEJS_20_X,
+    //   functionName: 'turbo-remote-cache-login-success',
+    //   handler: 'index.handler',
+    //   code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/dist/login-success')),
+    //   environment: {
+    //     TURBO_TOKEN: process.env.TURBO_TOKEN!,
+    //   },
+    // });
 
-    this.getUserInfoFunction = new lambda.Function(this, 'GetUserInfoFunction', {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      functionName: 'turbo-remote-cache-get-user-info',
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/dist/get-user-info')),
-    });
+    // this.getUserInfoFunction = new lambda.Function(this, 'GetUserInfoFunction', {
+    //   runtime: lambda.Runtime.NODEJS_20_X,
+    //   functionName: 'turbo-remote-cache-get-user-info',
+    //   handler: 'index.handler',
+    //   code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/dist/get-user-info')),
+    // });
 
     props.artifactsBucket.grantRead(this.artifactQueryFunction);
     props.artifactsBucket.grantReadWrite(this.recordEventsFunction);
